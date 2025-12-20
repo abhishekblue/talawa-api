11e8eea81c7a8b9cfec90937844d5bc116aa37b4
abihshekblue <jainabhishekjain007@gmail.com>
2025-12-20 15:26:22 +0530

    updating setupDocker

--- src/install/setupDocker.ts
+++ src/install/setupDocker.ts
@@ -7,14 +7,14 @@ const ENV_DEV_SOURCE = path.join(ROOT_DIR, 'envFiles', '.env.devcontainer');
 const ENV_CI_SOURCE = path.join(ROOT_DIR, 'envFiles', '.env.ci');
 const ENV_DEST = path.join(ROOT_DIR, '.env');
 
-const needsSudoForDocker = (): boolean => {
-  try {
-    execSync('docker ps', { stdio: 'ignore' });
-    return false;
-  } catch {
-    return true;
-  }
-};
+// const needsSudoForDocker = (): boolean => {
+//   try {
+//     execSync('docker ps', { stdio: 'ignore' });
+//     return false;
+//   } catch {
+//     return true;
+//   }
+// };
 
 // const runCommand = (command: string) => {
 //   try {
@@ -56,16 +56,20 @@ async function main() {
   console.log('\n📦 Installing DevContainer CLI...');
   execSync('pnpm install -g @devcontainers/cli');
 
-  if (process.platform === 'linux' && needsSudoForDocker()) {
-    console.log('\n🔧 Adding user to docker group...');
+  // if (process.platform === 'linux') {
+  //   console.log('\n🔧 Adding user to docker group...');
 
-    execSync('sudo usermod -a -G docker $USER}');
-    console.log('==========sud command check 1===========')
-    execSync ('sudo su $USER -')
-    console.log('==========sud command check pass===========')
+  //   console.log('==========sudo command check 1===========')
+  //   execSync('sudo usermod -a -G docker $USER');
+  //   execSync ('sudo su $USER -')
+  //   console.log('==========sudo command check pass===========')
+    
+  // }
 
-  }
 
+  execSync('sudo usermod -a -G docker $USER');
+  execSync ('sudo su $USER -')
+  
   console.log('\n🐳 Building DevContainer...');
   execSync('devcontainer build --workspace-folder .');
 
